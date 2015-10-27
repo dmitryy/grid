c1584745ccdf57b4c761c70f803b88cba4a88d4e
dmitryy <dmitry@ruswizards.com>
2015-10-27 16:46:49 +0300

    basic layout

---
+++ js/filter.js
@@ -0,0 +1,45 @@
+﻿var Filter = function () {
+    // This is dummy filter class, which will do following:
+    // - working with service
+    // - returns filtered data 
+    // - apply filters to data (through service or not)
+
+    this.getData = function (pageIndex, pageSize, orderBy, reverse, callback) {
+
+        fakeData.sort(function (a, b) {
+            if (a[orderBy] < b[orderBy])
+                return -1;
+            if (a[orderBy] > b[orderBy])
+                return 1;
+            return 0;
+        });
+
+        if (reverse) {
+            fakeData.reverse();
+        }
+
+        var data = fakeData.subarray(pageIndex * pageSize, pageIndex * pageSize + pageSize);
+
+        // return data with delay
+        setTimeout(function () {
+            callback.call(null, { pageIndex: pageIndex, pageSize: pageSize, data: data });
+        }, 100);
+    }
+}
+
+function clone(obj) {
+    if (null == obj || "object" != typeof obj) return obj;
+    var copy = obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+    }
+    return copy;
+}
+
+Array.prototype.subarray = function (start, end) {
+    if (!end) {
+        end = this.length;
+    }
+    var newArray = clone(this);
+    return newArray.slice(start, end);
+};
---
+++ js/grid.js
@@ -0,0 +1,73 @@
+﻿var Grid = function(filter, sortBy, template, rowTemplate, placeholder)
+{
+    this.filter = filter;
+    this.renderGrid = _.template($(template).html());
+    this.renderGridRow = _.template($(rowTemplate).html());
+    this.sortBy = sortBy;
+    this.sortReverse = false;
+    this.pageIndex = 0;
+    this.pageSize = 10;
+    this.pageLoaded = [];
+
+    this.init = function () {
+        this.sort();
+    }
+
+    this.sort = function () {
+        this.pageIndex = -1;
+        this.pageLoaded = [];
+        this.loadPage(0, $.proxy(this, 'pageLoadCallback'));
+    }
+
+    this.pageLoadCallback = function (data) {
+        $(placeholder).empty().append(this.renderGrid({
+            rows: this.getRowsHtml(data)
+        }));
+        this.setPage(0);
+        this.bindEvents();
+    }
+
+    this.progressiveLoadCallback = function (data) {
+        if (data && data.data && data.data.length) {
+            $(this.getRowsHtml(data)).insertAfter('table.grid tbody:last', placeholder);
+        }
+    }
+
+    this.bindEvents = function () {
+        var self = this;
+        $('table', placeholder).unbind().mousewheel(function () {
+            self.setPage(parseInt($('table tbody:onScreen:last', placeholder).attr('page')));
+        });
+        $('table th', placeholder).unbind().click(function () {
+            var column = $(this).attr('column');
+            self.sortReverse = column == self.sortBy && !self.sortReverse;
+            self.sortBy = column;
+            self.sort(self.sortBy, self.sortReverse);
+        });
+    }
+
+    this.setPage = function (index) {
+        if (this.pageIndex != index) {
+            this.pageIndex = index;
+            if ($.inArray(index + 1, this.pageLoaded) == -1) {
+                this.loadPage(index + 1, $.proxy(this, 'progressiveLoadCallback'));   // always load next page 
+            }
+        }
+    }
+
+    this.loadPage = function (index, callback) {
+        this.filter.getData(index, this.pageSize, this.sortBy, this.sortReverse, callback);
+    }
+
+    this.getRowsHtml = function (data) {
+        this.pageLoaded.push(data.pageIndex);
+        var rows = '<tbody page="' + data.pageIndex + '">';
+        for (var i = 0; i < data.data.length; i++) {
+            rows += this.renderGridRow({ row: data.data[i], page: data.pageIndex });
+        }
+        rows += '</tbody>';
+        return rows;
+    }
+
+    $($.proxy(this, 'init'));
+}
