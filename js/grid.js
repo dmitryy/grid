362fdd477bb3a36f8c25a5eef788bd17391dc4d8
dmitryy <dmitry@ruswizards.com>
2015-10-29 15:55:19 +0300

    selection and callbacks

--- js/grid.js
+++ js/grid.js
@@ -9,6 +9,9 @@
     this.pageSize = pageSize;
     this.pageLoaded = [];
 
+    this.clickCallback = null;
+    this.contextCallback = null;
+
     this.sort = function () {
         this.pageIndex = -1;
         this.pageLoaded = [];
@@ -23,11 +26,13 @@
         $('thead th[column=' + this.sortBy + ']', placeholder).addClass('sort').addClass(this.sortReverse ? 'desc' : '');
         this.setPage(0);
         this.bindEvents();
+        this.bindRowEvents();
     }
 
     this.progressiveLoadCallback = function (data) {
         if (data && data.data && data.data.length) {
             $(this.getRowsHtml(data)).insertAfter(placeholder + ' table.grid tbody:last');
+            this.bindRowEvents();
         }
     }
 
@@ -42,6 +47,32 @@
             self.sortBy = column;
             self.sort();
         });
+    }
+
+    this.bindRowEvents = function () {
+        var self = this;
+        $('table tr', placeholder).unbind()
+            .on('click', 'td', function (event) {
+                var tr = $(this).parent();
+                var id = tr.attr('key');
+                if (id) {
+                    self.rowClick(id, tr, event);
+                }
+                else {
+                    console.error('Key attribute is not defined in row template.')
+                }
+            })
+            .on('contextmenu', 'td', function (event) {
+                var tr = $(this).parent();
+                var id = tr.attr('key');
+                if (id) {
+                    self.rowRightClick(id, tr, event);
+                }
+                else {
+                    console.error('Key attribute is not defined in row template.')
+                }
+                return false;
+            });
     }
 
     this.setPage = function (index) {
@@ -61,16 +92,60 @@
         this.pageLoaded.push(data.pageIndex);
         var rows = '<tbody page="' + data.pageIndex + '">';
         for (var i = 0; i < data.data.length; i++) {
-            rows += this.renderGridRow({ row: data.data[i], page: data.pageIndex });
+            if (data.data[i]) {
+                rows += this.renderGridRow({ row: data.data[i], page: data.pageIndex });
+            }
         }
         rows += '</tbody>';
         return rows;
     }
 
     this.sort();
+
+    this.rowClick = function (id, tr, event) {
+        if ($(tr).hasClass('selected')) {
+            $(tr).removeClass('selected');
+        }
+        else {
+            $(tr).addClass('selected');
+        }
+        if (this.clickCallback) {
+            this.clickCallback(id, tr, event);
+        }
+    }
+
+    this.rowRightClick = function (id, tr, event) {
+        if (this.contextCallback) {
+            this.contextCallback(id, tr, event);
+        }
+    }
+
+    this.hideRowByKey = function (id) {
+        $('tr[key=' + id + ']').fadeOut();
+    }
+
+    // adds event handler for row click
+    // output params: id, tr, event
+    this.click = function (callback) {
+        this.clickCallback = callback;
+    }
+
+    // adds event handler for contextmenu
+    // output params: id, tr, event
+    this.context = function (callback) {
+        this.contextCallback = callback;
+    }
+
+    // public events
+    // - rowClick
+    // - rowContext
+    // - getRows
+    // - getSelected
+    // - setSelected
 }
 
 // TODO:
-// 2. row selection (optional)
-// 3. get selected rows as object
+// 1. row selection (optional)
+// 2. get selected rows as object
+// 3. click and context handlers
 // 4. scroll by dragging scroll bar
